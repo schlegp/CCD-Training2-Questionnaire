fb35a2104f13be9563722e2f9ae6689144ef19b2
Lukas Frensel <Lukas.Frensel@outlook.de>
2020-02-13 14:35:42 +0100

    install first ui part

---
+++ Questionnaire/Questionnaire/QuestionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Questionnaire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questionnaire
+{
+    public class QuestionHandler
+    {
+        public Interpreter interpreter;
+        public FileHandler fileHandler;
+        public QuestionHandler()
+        {
+            interpreter = new Interpreter();
+            fileHandler = new FileHandler();
+        }
+
+        public IEnumerable<Question> Provide()
+        {
+            var lines = fileHandler.ReadFile("questionnaire.txt");
+            var Questions = interpreter.Interpret(lines);
+            return Questions;
+        }
+
+    }
+}
