f9c5e701cbccc223aa0946455453ef234af616d1
Lukas Frensel <Lukas.Frensel@outlook.de>
2020-02-13 13:28:12 +0100

    start Interpreter

---
+++ Questionnaire/Questionnaire/Interpreter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questionnaire
+{
+    public class Interpreter
+    {
+        public bool IsQuestion(string line)
+        {
+            if (line[0] == '?')
+                return true;
+            return false;
+        }
+
+        public Answer InterpretAnswer(string line)
+        {
+            var answer = new Answer();
+            answer.RightAnswer = false;
+            if (line[0] == '*')
+                answer.RightAnswer = true;
+            if (answer.RightAnswer)
+            {
+                answer.AnswerText = line.Substring(1)
+            }
+            else
+            {
+                answer.AnswerText = line;
+            }
+            return answer;
+        }
+    }
+}
---
+++ Questionnaire/TestQuestionnaire/TestInterpreter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Questionnaire;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestQuestionnaire
+{
+    [TestClass]
+    public class TestInterpreter
+    {
+        private Interpreter testInterpreter;
+
+        public TestInterpreter()
+        {
+            testInterpreter = new Interpreter();
+        }
+
+
+        [TestMethod]
+        [DataRow("aösldkjfaölskdjföasldkf", false)]
+        [DataRow("?aösldkjfaölskdjföasldkf", true)]
+        public void TestIsQuestion(string line, bool expected)
+        {
+            var result = testInterpreter.IsQuestion(line);
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
