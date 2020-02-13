1fc8cda80655b808d559b543e8f2ac82ec100750
Lukas Frensel <Lukas.Frensel@outlook.de>
2020-02-13 15:05:27 +0100

    update ui

---
+++ Questionnaire/Questionnaire/Pages/QuestionComp.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Questionnaire.Models;
+
+namespace Questionnaire
+{
+    public partial class QuestionComp : ComponentBase
+    {
+        [Parameter]
+        public Question Question { get; set; }
+    }
+}
